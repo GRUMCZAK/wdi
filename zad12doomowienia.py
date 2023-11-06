3de205e76ca52a69f3b1830fb2e155e211423d78
GRUMCZAK <86237691+GRUMCZAK@users.noreply.github.com>
2023-11-06 19:39:39 +0100

    Update zad12doomowienia.py
    Poprzednio było tu coś totalnie innego

--- zad12doomowienia.py
+++ zad12doomowienia.py
@@ -1,20 +1,11 @@
-a=int(input())
-print(a)
-for x in range(1,a):
-    print(" ",end="")
-for x in range(0,a+1):
-    for i in range(1,a-x+1):
-        print(" ", end="")
-    for g in range(1,2+2*(x-1)):
-        if x==1:
-            print("X", end="")
-        else:
-            if g%2==0:
-                print("o", end="")
-            else:
-                print("*", end="")
-    print("\n",end="")
-    x=x-1
-for x in range (1,a):
-    print(" ",end="")
-print("U")
+f = float(0) #dwa poczatkowe wyrazy ciagu
+f1 = float(1)
+i = 1
+while 1==1: # nieskonczona petla
+    print("n=",end="")#numerowwanie kolejnych ilorazow
+    print(i,end=" ")
+    i+=1
+    print(float(f/f1)) # gdy zamienimy f i f1, to granica zmieni sie o 1
+    a=f1
+    f1=f1+f
+    f=a
